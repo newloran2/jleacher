6faa8f7148abd30031292ffc82032fd753a67dcf
Erik Clemente <newloran2@gmail.com>
2012-03-04 15:30:54 -0300

    adicionado classes para irc boot e dcc transfer

---
+++ src/main/java/br/com/newloran2/jleacher/boot/DccTransfer.java
@@ -0,0 +1,13 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.com.newloran2.jleacher.boot;
+
+/**
+ *
+ * @author newloran2
+ */
+public class DccTransfer {
+    
+}
